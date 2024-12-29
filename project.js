ce9843ec49e9a707d5e0ad453f33ed715f606019
Dhruv Dhankhar <dhruvdhkh@gmail.com>
2024-12-29 21:49:22 +0530

    update

--- project.js
+++ project.js
@@ -44,3 +44,17 @@ const deposit = () => {
       }
     }
   };
+
+
+  const getNumberOfLines = () => {
+    while (true) {
+      const lines = prompt("Enter the number of lines to bet on (1-3): ");
+      const numberOfLines = parseFloat(lines);
+  
+      if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+        console.log("Invalid number of lines, try again.");
+      } else {
+        return numberOfLines;
+      }
+    }
+  };// asking the user to enter the no. of line to bet on.
